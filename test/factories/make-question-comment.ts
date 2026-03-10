69d9831d71c71e9365b3466f19800fb7d70405cc
Mateus Kunrath <dev.mateuskunrath@gmail.com>
2026-03-09 21:50:19 -0300

    Realizado ajustes necessários em algumas factories e ajustado também testes e2e para utilizarem as factories implementadas

--- test/factories/make-question-comment.ts
+++ test/factories/make-question-comment.ts
@@ -1,3 +1,4 @@
+import { PrismaQuestionCommentMapper } from './../../src/infra/database/prisma/mappers/prisma-question-comment-mapper'
 import { faker } from '@faker-js/faker'
 
 import {
@@ -5,6 +6,8 @@ import {
   QuestionCommentProps,
 } from '@/domain/forum/enterprise/entities/question-comment'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { Injectable } from '@nestjs/common'
 
 export function makeQuestionComment(
   override: Partial<QuestionCommentProps> = {},
@@ -19,4 +22,21 @@ export function makeQuestionComment(
     },
     id,
   )
+}
+
+@Injectable()
+export class QuestionCommentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaQuestionComment(
+    data: Partial<QuestionCommentProps> = {},
+  ): Promise<QuestionComment> {
+    const questionComment = makeQuestionComment(data)
+
+    await this.prisma.comment.create({
+      data: PrismaQuestionCommentMapper.toPrisma(questionComment),
+    })
+
+    return questionComment
+  }
 }
