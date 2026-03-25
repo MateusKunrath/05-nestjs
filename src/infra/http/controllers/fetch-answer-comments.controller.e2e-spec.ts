4507f0ac924720f99d3bc535cc1ea5bc47a51e90
Mateus Kunrath <dev.mateuskunrath@gmail.com>
2026-03-25 20:43:57 -0300

    Implementação para retornar o autor dos comentários nas respostas pela controller

--- src/infra/http/controllers/fetch-answer-comments.controller.e2e-spec.ts
+++ src/infra/http/controllers/fetch-answer-comments.controller.e2e-spec.ts
@@ -39,7 +39,7 @@ describe('Fetch answer comments (E2E)', () => {
   })
 
   test('[GET] /answers/:answerId/comments', async () => {
-    const user = await studentFactory.makePrismaStudent()
+    const user = await studentFactory.makePrismaStudent({ name: 'John Doe' })
 
     const accessToken = jwt.sign({ sub: user.id.toString() })
 
@@ -74,9 +74,15 @@ describe('Fetch answer comments (E2E)', () => {
 
     expect(response.statusCode).toBe(200)
     expect(response.body).toEqual({
-      answerComments: expect.arrayContaining([
-        expect.objectContaining({ content: 'Comment 01' }),
-        expect.objectContaining({ content: 'Comment 02' }),
+      comments: expect.arrayContaining([
+        expect.objectContaining({
+          comment: expect.objectContaining({ content: 'Comment 01' }),
+          author: expect.objectContaining({ name: 'John Doe' }),
+        }),
+        expect.objectContaining({
+          comment: expect.objectContaining({ content: 'Comment 02' }),
+          author: expect.objectContaining({ name: 'John Doe' }),
+        }),
       ]),
     })
   })
