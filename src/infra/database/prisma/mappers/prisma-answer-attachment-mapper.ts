5032469294f2be8c678b94512b11097715d9639f
Mateus Kunrath <dev.mateuskunrath@gmail.com>
2026-03-25 19:14:01 -0300

    Implementada a adição e atualização de anexos no banco de dados para as respostas das perguntas

--- src/infra/database/prisma/mappers/prisma-answer-attachment-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-answer-attachment-mapper.ts
@@ -1,4 +1,7 @@
-import { Attachment as PrismaAttachment } from 'prisma/generated/prisma/client'
+import {
+  Prisma,
+  Attachment as PrismaAttachment,
+} from 'prisma/generated/prisma/client'
 import { AnswerAttachment } from '@/domain/forum/enterprise/entities/answer-attachment'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
@@ -15,5 +18,22 @@ export class PrismaAnswerAttachmentMapper {
       },
       new UniqueEntityID(raw.id),
     )
+  }
+
+  static toPrismaUpdateMany(
+    attachments: AnswerAttachment[],
+  ): Prisma.AttachmentUpdateManyArgs {
+    const attachmentsIds = attachments.map((a) => a.attachmentId.toString())
+
+    return {
+      where: {
+        id: {
+          in: attachmentsIds,
+        },
+      },
+      data: {
+        answerId: attachments[0].answerId.toString(),
+      },
+    }
   }
 }
